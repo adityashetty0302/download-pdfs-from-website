c5fdbed42e759ccae827678abc6486ee52f64e0c
adityashetty0302 <adityashetty0302@gmail.com>
2018-07-28 22:36:02 +0530

    Initial commit

---
+++ Download_pdfs_from_website.py
@@ -0,0 +1,34 @@
+import os
+import urllib.request
+import urllib.request
+
+try:
+    from bs4 import BeautifulSoup
+except Exception as e:
+    print("Pls install BeautifulSoup 4")
+
+url = input('[+] Enter the url: ')
+
+try:
+
+    i = 0
+    html = urllib.request.urlopen(url)
+    soup = BeautifulSoup(html.read(), 'html.parser')
+    for tag in soup.findAll('a', href=True):
+
+        if os.path.splitext(os.path.basename(tag['href']))[1] == '.pdf':
+            current = urllib.request.urlopen(tag['href'])
+
+            print("\n[*] Downloading: %s" % (os.path.basename(tag['href'])))
+
+            f = open(os.path.basename(tag['href']), "wb")
+            f.write(current.read())
+            f.close()
+            i += 1
+
+    print("\n[*] Downloaded %d files" % (i))
+    input("[+] Press any key to exit...")
+
+
+except Exception as e:
+    print(e)
